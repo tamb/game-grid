fc6538b158daff12494da5d8c32eee9ec9f0e0f5
Tom Saporito <tamb89@protonmail.com>
2021-10-10 22:55:47 -0400

    Adds typescript and js react demos

---
+++ demos/ts/src/GameGridDemo.ts
@@ -0,0 +1,75 @@
+import GameGrid from "@tamb/gamegrid";
+
+const tileTypeEnum = {
+  OPEN: "open",
+  INTERACTIVE: "interactive",
+  BARRIER: "barrier",
+};
+
+const matrix = [
+  [
+    {
+      type: tileTypeEnum.OPEN,
+      cellAttributes: [["data-butt", "sauce"]],
+      renderFunction: function () {
+        return document.createElement("input");
+      },
+    },
+    { type: tileTypeEnum.OPEN },
+    { type: tileTypeEnum.OPEN },
+    {
+      type: tileTypeEnum.INTERACTIVE,
+      cellAttributes: [["data-cell-type", "interactive"]],
+    },
+  ],
+  [
+    { type: tileTypeEnum.OPEN },
+    { type: tileTypeEnum.OPEN },
+    { type: tileTypeEnum.OPEN },
+    {
+      type: tileTypeEnum.BARRIER,
+      cellAttributes: [["data-cell-type", "barrier"]],
+    },
+  ],
+  [
+    { type: tileTypeEnum.OPEN },
+    { type: tileTypeEnum.OPEN },
+    { type: tileTypeEnum.OPEN },
+    {
+      type: tileTypeEnum.BARRIER,
+      cellAttributes: [["data-cell-type", "barrier"]],
+    },
+    { type: tileTypeEnum.OPEN },
+    { type: tileTypeEnum.OPEN },
+  ],
+  [
+    { type: tileTypeEnum.OPEN },
+    { type: tileTypeEnum.OPEN },
+    {
+      type: tileTypeEnum.BARRIER,
+      cellAttributes: [["data-cell-type", "barrier"]],
+    },
+    { type: tileTypeEnum.OPEN },
+  ],
+];
+
+export function createGrid() {
+  return new GameGrid("#grid", {
+    matrix,
+    options: {
+      infinite_y: true,
+      infinite_x: true,
+      clickable: true,
+      arrow_controls: true,
+      wasd_controls: true,
+      callbacks: {
+        LIMIT: function (x: GameGrid) {
+          console.log("callback for LIMIT", x);
+        },
+        WRAP_Y: function (x : GameGrid) {
+          console.log("callback WRAP_Y", x);
+        },
+      },
+    },
+  });
+}
