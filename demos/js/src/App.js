b00c40faffb1af02e40950db4a223a0302f8f187
Tom Saporito <tamb89@protonmail.com>
2021-10-10 22:57:53 -0400

    adds react ts demos

--- demos/js/src/App.js
+++ demos/js/src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { createGrid } from "./GameGridDemo";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.grid = null;
+  }
+
+  componentDidMount() {
+    this.grid = createGrid();
+    this.grid.render();
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>gamegrid</h1>
+        <div>JavaScript</div>
+        <div id="grid"></div>
+      </div>
+    );
+  }
 }
 
 export default App;
