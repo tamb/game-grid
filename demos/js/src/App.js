00e290dfafe24ecb6270c9b083e1f8677f5a9941
Tom Saporito <tamb89@protonmail.com>
2021-10-11 22:39:08 -0400

    adds buttons to js demo

--- demos/js/src/App.js
+++ demos/js/src/App.js
@@ -4,12 +4,15 @@ import { createGrid } from "./GameGridDemo";
 class App extends React.Component {
   constructor(props) {
     super(props);
-    this.grid = null;
+    this.state = {
+      grid: null
+    };
   }
 
   componentDidMount() {
-    this.grid = createGrid();
-    this.grid.render();
+    const grid = createGrid();
+    grid.render();
+    this.setState({grid});
   }
 
   render() {
@@ -17,7 +20,37 @@ class App extends React.Component {
       <div>
         <h1>gamegrid</h1>
         <div>JavaScript</div>
-        <div id="grid"></div>
+        <div style={{ display: "flex" }}>
+          <div
+            style={{
+              width: "25%",
+              paddingLeft: "10%",
+            }}
+            id="grid"
+          ></div>
+          <div
+            style={{
+              width: "50%",
+            }}
+          >
+            {this.state.grid && (
+              <React.Fragment>
+                <button onClick={this.state.grid.moveUp.bind(this.state.grid)} type="button">
+                  UP
+                </button>
+                <button onClick={this.state.grid.moveRight.bind(this.state.grid)} type="button">
+                  RIGHT
+                </button>
+                <button onClick={this.state.grid.moveDown.bind(this.state.grid)} type="button">
+                  DOWN
+                </button>
+                <button onClick={this.state.grid.moveLeft.bind(this.state.grid)} type="button">
+                  LEFT
+                </button>
+              </React.Fragment>
+            )}
+          </div>
+        </div>
       </div>
     );
   }
