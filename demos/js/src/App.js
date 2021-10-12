20b9119088b177883967f73b3fb6fba40a9a0f15
Tom Saporito <tamb89@protonmail.com>
2021-10-12 19:50:37 -0400

    adds bootstrap

--- demos/js/src/App.js
+++ demos/js/src/App.js
@@ -5,49 +5,59 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      grid: null
+      grid: null,
     };
   }
 
   componentDidMount() {
     const grid = createGrid();
     grid.render();
-    this.setState({grid});
+    this.setState({ grid });
   }
 
   render() {
     return (
-      <div>
-        <h1>gamegrid</h1>
-        <div>JavaScript</div>
-        <div style={{ display: "flex" }}>
-          <div
-            style={{
-              width: "25%",
-              paddingLeft: "10%",
-            }}
-            id="grid"
-          ></div>
-          <div
-            style={{
-              width: "50%",
-            }}
-          >
+      <div className="container">
+        <div className="row">
+          <div className="col-12">
+            <h1>gamegrid</h1>
+            <div>JavaScript</div>
+          </div>
+          <div className="col-6">
+            <div id="grid"></div>
+          </div>
+          <div className="col-6">
             {this.state.grid && (
-              <React.Fragment>
-                <button onClick={this.state.grid.moveUp.bind(this.state.grid)} type="button">
+              <div className="d-flex flex-wrap justify-content-center">
+                <button
+                  className="w-75 m-3 btn btn-primary"
+                  onClick={this.state.grid.moveUp.bind(this.state.grid)}
+                  type="button"
+                >
                   UP
                 </button>
-                <button onClick={this.state.grid.moveRight.bind(this.state.grid)} type="button">
-                  RIGHT
-                </button>
-                <button onClick={this.state.grid.moveDown.bind(this.state.grid)} type="button">
-                  DOWN
-                </button>
-                <button onClick={this.state.grid.moveLeft.bind(this.state.grid)} type="button">
+                <button
+                  className="w-50 btn btn-primary"
+                  onClick={this.state.grid.moveLeft.bind(this.state.grid)}
+                  type="button"
+                >
                   LEFT
                 </button>
-              </React.Fragment>
+                <button
+                  className="w-50 btn btn-primary"
+                  onClick={this.state.grid.moveRight.bind(this.state.grid)}
+                  type="button"
+                >
+                  RIGHT
+                </button>
+                <button
+                  className="w-75 m-3 btn btn-primary"
+                  onClick={this.state.grid.moveDown.bind(this.state.grid)}
+                  type="button"
+                >
+                  DOWN
+                </button>
+              </div>
             )}
           </div>
         </div>
