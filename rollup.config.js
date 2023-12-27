8c5f2b453cc14efd2e8a1b6a66860f59db747f59
tamb <t.saporito@gmail.com>
2023-12-27 00:07:47 -0500

    updates rollup config and adds terser removes postcss

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,7 @@
 const resolve = require("@rollup/plugin-node-resolve");
 const commonjs = require("@rollup/plugin-commonjs");
 const typescript = require("@rollup/plugin-typescript");
-const postcss = require("rollup-plugin-postcss");
+const terser = require("@rollup/plugin-terser");
 
 module.exports = {
   input: "src/index.ts",
@@ -9,6 +9,7 @@ module.exports = {
     file: "dist/main.js",
     format: "umd",
     name: "GameGrid",
+    sourcemap: true,
   },
-  plugins: [resolve(), commonjs(), typescript(), postcss()],
+  plugins: [resolve(), commonjs(), typescript(), terser()],
 };
