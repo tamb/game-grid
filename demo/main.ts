d8cc0bfda42328b7a8522c74a97a7332dd171909
tamb <t.saporito@gmail.com>
2024-01-07 13:07:31 -0500

    Update demos to exist in their own tabs

--- demo/main.ts
+++ demo/main.ts
@@ -2,12 +2,12 @@ import './styles.scss';
 import { attachListeners } from './eventListeners.js';
 import { renderMaze } from './maze.js';
 import { createGrid } from './grid1.js';
-import { createCoinGrid } from './collectionMaze.js';
+import { setupCoinGrid } from './collectionMaze.js';
 
 document.addEventListener('DOMContentLoaded', function () {
   attachListeners();
   createGrid();
   renderMaze('#maze', 50);
   renderMaze('#maze2', 10);
-  createCoinGrid();
+  setupCoinGrid();
 });
