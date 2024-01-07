c775950a62b3710e8e924c257967ab73c7a2f138
tamb <t.saporito@gmail.com>
2024-01-07 16:07:47 -0500

    updates gitignore to ignore dist

--- dist/utils.d.ts
+++
@@ -1,5 +0,0 @@
-export declare function getKeyByValue(object: any, value: string): string | undefined;
-export declare function renderAttributes(el: HTMLElement, tuples: [string, string][]): void;
-export declare function getCoordsFromElement(el: HTMLElement): number[] | undefined;
-export declare function fireCustomEvent(eventName: string, data?: any): void;
-export declare function insertStyles(): void;
