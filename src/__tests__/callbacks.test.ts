9b2550173c9fbbc8ebf4313bbd8ac05522a7732b
tamb <t.saporito@gmail.com>
2024-01-19 02:06:25 -0500

    uses in memory grid

--- src/__tests__/callbacks.test.ts
+++ src/__tests__/callbacks.test.ts
@@ -2,7 +2,7 @@ import GameGrid from '../index';
 import { matrix } from '../__mocks__/matrix';
 
 describe('Callbacks', () => {
-  let renderedGrid: GameGrid;
+  let memoryGrid: GameGrid;
   let callbacks = {
     onBlock: jest.fn(),
     onBoundary: jest.fn(),
@@ -13,77 +13,67 @@ describe('Callbacks', () => {
     onWrap: jest.fn(),
   };
 
-  beforeAll(() => {
-    document.body.insertAdjacentHTML('afterbegin', '<div id="root"></div>');
-  });
-
   beforeEach(() => {
-    renderedGrid = new GameGrid(
-      {
-        matrix,
-        options: {
-          callbacks,
-        },
+    memoryGrid = new GameGrid({
+      matrix,
+      options: {
+        callbacks,
       },
-      document.getElementById('root')!,
-    );
+    });
   });
 
   afterEach(() => {
-    renderedGrid.destroy();
-    renderedGrid = null!;
+    memoryGrid.destroy();
+    memoryGrid = null!;
   });
 
   test('onMove callback is called', () => {
-    renderedGrid.moveLeft();
+    memoryGrid.moveLeft();
     expect(callbacks.onMove).toHaveBeenCalled();
   });
 
   test('onLand callback is called', () => {
-    renderedGrid.moveRight();
+    memoryGrid.moveRight();
     expect(callbacks.onLand).toHaveBeenCalled();
   });
 
   test('onBlock callback is called', () => {
-    renderedGrid.moveRight();
-    renderedGrid.moveRight();
+    memoryGrid.moveRight();
+    memoryGrid.moveRight();
 
     expect(callbacks.onBlock).toHaveBeenCalled();
   });
 
   test('onCollide callback is called', () => {
-    renderedGrid.moveRight();
-    renderedGrid.moveDown();
+    memoryGrid.moveRight();
+    memoryGrid.moveDown();
     expect(callbacks.onCollide).toHaveBeenCalled();
   });
 
   test('onDettach callback is called', () => {
-    renderedGrid.moveRight();
-    renderedGrid.moveDown();
-    renderedGrid.moveDown();
+    memoryGrid.moveRight();
+    memoryGrid.moveDown();
+    memoryGrid.moveDown();
     expect(callbacks.onDettach).toHaveBeenCalled();
   });
 
   test('onBoundary callback is called', () => {
-    const grid = new GameGrid(
-      {
-        matrix,
-        options: {
-          infiniteX: false,
-          infiniteY: false,
-          callbacks: {
-            onBoundary: callbacks.onBoundary,
-          },
+    const grid = new GameGrid({
+      matrix,
+      options: {
+        infiniteX: false,
+        infiniteY: false,
+        callbacks: {
+          onBoundary: callbacks.onBoundary,
         },
       },
-      document.getElementById('root')!,
-    );
+    });
     grid.moveUp();
     expect(callbacks.onBoundary).toHaveBeenCalled();
   });
 
   test('onWrap callback is called', () => {
-    renderedGrid.moveRight();
+    memoryGrid.moveRight();
     expect(callbacks.onWrap).toHaveBeenCalled();
   });
 });
